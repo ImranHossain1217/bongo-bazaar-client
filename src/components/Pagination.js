13432e73be45e9dcbc7fcacd6d8c8e5158cc0429
Rafi <imranhossainemon1717@gmail.com>
2023-05-11 17:21:05 +0600

    create pagination

---
+++ src/components/Pagination.js
@@ -0,0 +1,77 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { AiOutlineDoubleRight, AiOutlineDoubleLeft } from "react-icons/ai";
+
+const Pagination = ({ page, perPage, count, path, theme }) => {
+  const totalLinks = Math.ceil(count / perPage);
+  let startLoop = page;
+  let diff = totalLinks - page;
+  if (diff <= 3) {
+    startLoop = totalLinks - 3;
+  }
+  let endLoop = startLoop + 3;
+  if (startLoop <= 0) {
+    startLoop = 1;
+  }
+  const links = () => {
+    const allLinks = [];
+    for (let i = startLoop; i <= endLoop; i++) {
+      allLinks.push(
+        <li key={i} className="pagination-li">
+          <Link
+            className={` ${
+              theme === "light" ? "pagination-link-light" : "pagination-link"
+            }  ${page === i && "bg-indigo-500 text-white"}`}
+            to={`/dashboard/categories/${i}`}
+          >
+            {i}
+          </Link>
+        </li>
+      );
+    }
+    return allLinks;
+  };
+  const next = () => {
+    if (page < totalLinks) {
+      return (
+        <li className="pagination-li">
+          <Link
+            className={`${
+              theme === "light" ? "pagination-link-light" : "pagination-link"
+            }`}
+            to={`/dashboard/categories/${page + 1}`}
+          >
+            <AiOutlineDoubleRight className="text-xl" />
+          </Link>
+        </li>
+      );
+    }
+  };
+  const prev = () => {
+    if (page > 1) {
+      return (
+        <li className="pagination-li">
+          <Link
+            className={`${
+              theme === "light" ? "pagination-link-light" : "pagination-link"
+            }`}
+            to={`/dashboard/categories/${page - 1}`}
+          >
+            <AiOutlineDoubleLeft className="text-xl" />
+          </Link>
+        </li>
+      );
+    }
+  };
+  return (
+    count > perPage && (
+      <ul className="flex mt-2">
+        {prev()}
+        {links()}
+        {next()}
+      </ul>
+    )
+  );
+};
+
+export default Pagination;
--- src/screens/dashboard/Categories.js
+++ src/screens/dashboard/Categories.js
@@ -7,10 +7,14 @@ import { useDispatch, useSelector } from "react-redux";
 import { removeMsg } from "../../redux/reducers/globalReducer";
 import { useGetCategoriesQuery } from "../../redux/api/categoryApi";
 import Spinner from "../../components/Spinner";
+import Pagination from "../../components/Pagination";
 
 const Categories = () => {
-  const { page } = useParams();
-  const { data = [], isLoading } = useGetCategoriesQuery(page ? page : 1);
+  let { page } = useParams();
+  if (!page) {
+    page = 1;
+  }
+  const { data = [], isLoading } = useGetCategoriesQuery(page);
   const { success } = useSelector((state) => state.globalReducer);
   const dispatch = useDispatch();
 
@@ -38,38 +42,45 @@ const Categories = () => {
         <Spinner />
       ) : (
         data?.categories.length > 0 && (
-          <div>
-            <table className="w-full bg-gray-900 rounded-md">
-              <thead>
-                <tr className="border-b border-gray-800 text-left">
-                  <th className="p-3 uppercase text-gray-500 font-medium">
-                    name
-                  </th>
-                  <th className="p-3 uppercase text-gray-500 font-medium">
-                    edit
-                  </th>
-                  <th className="p-3 uppercase text-gray-500 font-medium">
-                    delete
-                  </th>
-                </tr>
-              </thead>
-              <tbody>
-                {data?.categories?.map((category) => (
-                  <tr className="odd:bg-gray-800" key={category._id}>
-                    <td className="p-3 capitalize font-normal text-gray-100">
-                      {category.name}
-                    </td>
-                    <td className="p-3 capitalize font-normal text-gray-100">
+          <>
+            <div>
+              <table className="w-full bg-gray-900 rounded-md">
+                <thead>
+                  <tr className="border-b border-gray-800 text-left">
+                    <th className="p-3 uppercase text-gray-500 font-medium">
+                      name
+                    </th>
+                    <th className="p-3 uppercase text-gray-500 font-medium">
                       edit
-                    </td>
-                    <td className="p-3 capitalize font-normal text-gray-100">
+                    </th>
+                    <th className="p-3 uppercase text-gray-500 font-medium">
                       delete
-                    </td>
+                    </th>
                   </tr>
-                ))}
-              </tbody>
-            </table>
-          </div>
+                </thead>
+                <tbody>
+                  {data?.categories?.map((category) => (
+                    <tr className="odd:bg-gray-800" key={category._id}>
+                      <td className="p-3 capitalize font-normal text-gray-100">
+                        {category.name}
+                      </td>
+                      <td className="p-3 capitalize font-normal text-gray-100">
+                        edit
+                      </td>
+                      <td className="p-3 capitalize font-normal text-gray-100">
+                        delete
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
+            <Pagination
+              page={parseInt(page)}
+              perPage={data.perPage}
+              count={data.count}
+            />
+          </>
         )
       )}
     </Wrapper>
